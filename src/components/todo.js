50bb9233bef1efa73bafd77ed988ed7ecefefbce
Alisson <alisson_942@outlook.com>
2020-01-08 21:15:56 -0300

    first-commit

---
+++ src/components/todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const Todo = () => (
+	<View>
+		<Text>Olá</Text>
+	</View>
+);
+
+export default Todo;
